4cd41ef755a40d42a9affdd451d54bdf42b66caf
Vanja Kostic <vanjakostic03@gmail.com>
2024-06-30 19:18:00 +0200

    [ADD] - artist classes

---
+++ Project-SIMS/src/model/Artist.java
@@ -0,0 +1,5 @@
+public abstract class Artist {
+
+    protected String Id;
+    
+}
---
+++ Project-SIMS/src/model/Bend.java
@@ -0,0 +1,5 @@
+public class Bend extends Artist{
+    private String Name;
+    private String Description;
+    private DateTime DateOfEstablishment;
+}
